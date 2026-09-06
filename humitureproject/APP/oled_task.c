528011a28fd70949d446b1dbd248f6218de0a538
LIUAA-Z <3340472599@qq.com>
2026-09-06 18:46:59 +0800

    不知道为啥，啥都没改，数据又正常了，串口输出的也正常了，上一个串口和oled都不正常

--- humitureproject/APP/oled_task.c
+++ humitureproject/APP/oled_task.c
@@ -26,14 +26,17 @@ void oled_display_task(void *arg)
 		xQueueReceive(Queue_humiture_handle,rebuf,portMAX_DELAY);
 
 
+		OLED_ShowString(1,1,"shidu:");
+		OLED_ShowNum(1,7,rebuf[0],2);
+		OLED_ShowChar(1, 10, '.');
+		OLED_ShowNum(1,11,rebuf[1],2);
+		OLED_ShowString(1,13,"%");
 
-		OLED_ShowNum(1,1,rebuf[0],2);
-		OLED_ShowChar(1, 3, '.');
-		OLED_ShowNum(1,4,rebuf[1],2);
-
-		OLED_ShowNum(2,1,rebuf[2],2);
-		OLED_ShowChar(2, 3, '.');
-		OLED_ShowNum(2,4,rebuf[3],2);
+		OLED_ShowString(2,1,"wendu:");
+		OLED_ShowNum(2,7,rebuf[2],2);
+		OLED_ShowChar(2, 10, '.');
+		OLED_ShowNum(2,11,rebuf[3],2);
+		OLED_ShowString(2,13,"C");
 
 		vTaskDelay(pdMS_TO_TICKS(10));
 	}
